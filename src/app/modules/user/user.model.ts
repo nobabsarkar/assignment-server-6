7f306c34fbf999b06f781ab92efb96221acecf74
nobabsarkar <nobabsarkar2016@gmail.com>
2025-01-26 20:52:08 +0600

    try to error handling

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -4,7 +4,7 @@ import { TUser } from "./user.interface";
 const userSchema = new Schema<TUser>({
   name: { type: String, required: [true, "Name is Required"] },
   image: { type: String, required: [true, "Image is Required"] },
-  email: { type: String, required: [true, "Email is Required"] },
+  email: { type: String, required: [true, "Email is Required"], unique: true },
   password: { type: String, required: [true, "Password is Required"] },
   role: { type: String, default: "USER", ENUM: ["USER", "ADMIN"] },
 });
